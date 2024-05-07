8c1d26fbe16e91549918e7bb848bfdf7a96d029d
ahmad1925 <ahmadqasim1925@gmail.com>
2024-05-07 14:31:20 +0500

    final

---
+++ currency/tailwind.config.js
@@ -0,0 +1,12 @@
+import 'tailwindcss/tailwind.css';
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
